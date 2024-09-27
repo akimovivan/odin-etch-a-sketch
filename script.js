76b657799e0b6b23e71a24c2ec51c01367c57940
uvaw <akimovivan13@gmail.com>
2024-09-27 12:28:43 +0300

    add randomized color on interaction

--- script.js
+++ script.js
@@ -18,14 +18,22 @@ function addSquares(numOfElements) {
     }
 }
 
+function randomRgba() {
+    let o = Math.round, r = Math.random, s = 255;
+    return `rgba(${o(r() * s)},${o(r() * s)},${r().toFixed(1)})`;
+}
 resetButton.addEventListener("click", () => {
     grid.innerHTML = '';
     let size = parseInt(prompt("Select grid size"));
+    if (size > 100) {
+        alert("Grid is too big");
+    }
     addSquares(size);
 });
 
 grid.addEventListener("mouseenter", event => {
     if (event.target.classList.contains('square') && event.ctrlKey) {
-        event.target.classList.add('hovered');
+        // event.target.classList.add('hovered');
+        event.target.style.backgroundColor = randomRgba();
     }
 }, true);
